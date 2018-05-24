a8ca22f09d88728dc6da18c25b2fb4b2905c37a5
shannon mcgarley <shannon.mcgarley@digital.hmrc.gov.uk>
2018-05-24 11:21:49 +0100

    3 tests pass

---
+++ src/main/scala/AlanPartridge.scala
@@ -0,0 +1,11 @@
+object AlanPartridge extends App {
+
+  def partridge(list: List[String]) : String ={
+    if (list.contains("PearTree")){
+      "Mine's a Pint!"
+    }else {
+      "Lynn, I've pierced my foot on a spike!!"
+    }
+  }
+
+}
---
+++ src/test/scala/AlanPartridgeSpec.scala
@@ -0,0 +1,19 @@
+import org.scalatest._
+
+class AlanPartridgeSpec extends WordSpec with MustMatchers {
+
+  "AlanPartridge" must {
+
+    "return 'Lynn, I've pierced my foot on a spike!!' when given an empty list" in {
+      AlanPartridge.partridge(List()) mustEqual "Lynn, I've pierced my foot on a spike!!"
+    }
+    "return 'Lynn, I've pierced my foot on a spike!!' when given a list of non AP terms" in {
+      AlanPartridge.partridge(List("Shannon")) mustEqual "Lynn, I've pierced my foot on a spike!!"
+    }
+    "return 'Mine's a Pint!' when given a list of one AP term" in {
+      AlanPartridge.partridge(List("PearTree")) mustEqual "Mine's a Pint!"
+    }
+
+  }
+
+}
