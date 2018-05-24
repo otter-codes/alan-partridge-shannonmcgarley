c6c56dd1ff003db6cb75800e52ac74bda82801a4
shannon mcgarley <shannon.mcgarley@digital.hmrc.gov.uk>
2018-05-24 14:40:23 +0100

    part 2 nearly done, can break vvvv easy

---
+++ src/main/scala/AppleTurnover.scala
@@ -0,0 +1,12 @@
+object AppleTurnover {
+  def turnoverTemp(temp : Any) : String = {
+    val tempAsInt = temp.toString.toInt
+    val tempSquared = tempAsInt*tempAsInt
+    if (tempSquared < 1000) {
+      "Help yourself to a honeycomb Yorkie for the glovebox."
+    }else{
+      "It's hotter than the sun!!"
+    }
+  }
+
+}
